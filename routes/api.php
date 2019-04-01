e745fc28efb2d3d395e7de2afd382ec97ef61091
mouhssine el ghazzali <mouhssine.elghazzali@gmail.com>
2019-04-01 11:52:49 +0100

    JWT Package

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,19 @@ Route::apiresource('/question/{question}/reply','ReplyController');
 
 Route::post('/like/{reply}','LikeController@LikeIt');
 Route::delete('/like/{reply}','LikeController@UnLikeIt');
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
 
 
 
